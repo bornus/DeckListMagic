7321935504a5a1ae22dde79eac0512b5426e642a
florent_bellier <bornus.fb@gmail.com>
2020-07-15 15:35:13 +0200

    init base CDK

---
+++ Back/src/CdkDeckListMagic/CdkDeckListMagicStack.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using Amazon.CDK;
+using Amazon.CDK.AWS.APIGateway;
+using Amazon.CDK.AWS.Lambda;
+using Amazon.CDK.AWS.S3;
+
+namespace CdkAvatar
+{
+    public class CdkAvatarStack : Stack
+    {
+        internal CdkAvatarStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            var bucket = Bucket.FromBucketArn(this, "bornusS3", "arn:aws:s3:::bornus");
+
+            // The code that defines your stack goes here
+            Function fn = new Function(this, "GetAvatar", new FunctionProps
+            {
+                Runtime = Runtime.DOTNET_CORE_3_1,
+                Code = Code.FromAsset("./GetAvatar/bin/Release/netcoreapp3.1/GetAvatar.zip"),
+                Handler = "GetAvatar::GetAvatar.Function::FunctionHandler",
+                Timeout = Duration.Seconds(30),
+                Environment = new Dictionary<string, string>
+                {
+                    {"BUCKET", bucket.BucketName}
+                }
+            });
+
+            bucket.GrantRead(fn);
+
+            var api = new RestApi(this, "Avatar", new RestApiProps
+            {
+                RestApiName = "Avatar",
+                Description = "Avatar's services",
+                BinaryMediaTypes = new []{"*/*"},
+            });
+
+            var getWidgetsIntegration = new LambdaIntegration(fn, new LambdaIntegrationOptions
+            {
+                RequestTemplates = new Dictionary<string, string>{
+                    { "application/json", "{ 'statusCode', '200'}" }
+                },
+            });
+
+            api.Root.AddMethod("GET", getWidgetsIntegration);
+
+            //var widget = api.Root.AddResource("{id}");
+
+        }
+    }
+}
---
+++ Back/src/CdkDeckListMagic/Program.cs
@@ -0,0 +1,28 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CdkAvatar
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            Amazon.CDK.Environment makeEnv(string account, string region)
+            {
+                return new Amazon.CDK.Environment
+                {
+                    Account = account,
+                    Region = region
+                };
+            }
+
+            var envEU = makeEnv(account: "722855790333", region: "eu-west-1");
+
+            var app = new App();
+            new CdkDeckListMagicStack(app, "CdkAvatarStack", new StackProps{Env = envEU});
+            app.Synth();
+        }
+    }
+}
