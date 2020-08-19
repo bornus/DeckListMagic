aa211385cf2e57db18d3f00b23435d783b8a881d
Shinriel <the.shinriel@gmail.com>
2020-08-19 21:26:20 +0200

    Re fix oO deploy

--- Front/deploy.sh
+++ Front/deploy.sh
@@ -1,6 +1,6 @@
 npm i
 npm run build
 
-aws2 s3 rm --recursive s3://elfenthlis-decklist-front
-aws2 s3 cp --recursive ./build s3://elfenthlis-decklist-front
-aws2 cloudfront create-invalidation --distribution-id EU5J1Y9PQOFTD --paths "/*"
+aws s3 rm --recursive s3://elfenthlis-decklist-front
+aws s3 cp --recursive ./build s3://elfenthlis-decklist-front
+aws cloudfront create-invalidation --distribution-id EU5J1Y9PQOFTD --paths "/*"
